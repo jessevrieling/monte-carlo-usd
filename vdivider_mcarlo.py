98024c0a2a47cb2ed49f773cdf49c10675baa946
Jesse <jessevrieling@gmail.com>
2025-11-07 20:42:39 +0100

    Added code to write data to csv file

--- vdivider_mcarlo.py
+++ vdivider_mcarlo.py
@@ -1,15 +1,28 @@
+import csv
 import res_util as ru
 
 def voltdivout(vin, r1_nom, r2_nom, stddevr1, stddevr2, n):
 	vout = []
 
-	for i in range(0, n):
+	for i in range(n):
 		r1 = ru.randrnorm(r1_nom, stddevr1)
 		r2 = ru.randrnorm(r2_nom, stddevr2)
 
 		vout.append(ru.vdivout(vin, r1, r2))
 
 	return vout
+
+def write_csv(arr):
+	data = []
+
+	for i in range(len(arr)):
+		data.append({'n': i, 'vout': arr[i]})
+
+	with open('simulation.csv', 'w', newline='') as csvfile:
+		fieldnames = ['n', 'vout']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		writer.writerows(data)
 
 vin = 100
 r1_nom = 200
@@ -24,4 +37,6 @@ print(f"stddev R1: {stddevr1}")
 print(f"stddev R2: {stddevr2}\n")
 
 vout = voltdivout(100, r1_nom, r2_nom, stddevr1, stddevr2, 10)
-print(vout)
+print("creating csv...")
+write_csv(vout)
+print("done");
