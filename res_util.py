dbc9b7c33f09006e9337bb411c58250b227d7379
Jesse <jessevrieling@gmail.com>
2025-11-07 20:28:02 +0100

    Forgot to factor with voltage

--- res_util.py
+++ res_util.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def vdivout(vin, r1, r2):
-	return r2 / (r1 + r2)
+	return r2 / (r1 + r2) * vin
 
 def randrnorm(r, stddev):
 	return np.random.normal(loc=r, scale=stddev, size=1)[0]
