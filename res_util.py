884b174aa3c5a421ea8866a3a8a74a819e8f1b6f
Jesse <jessevrieling@gmail.com>
2025-11-08 15:30:10 +0100

    Added function to calculate stddev based on tolerance percentage

--- res_util.py
+++ res_util.py
@@ -3,5 +3,8 @@ import numpy as np
 def vdivout(vin, r1, r2):
 	return r2 / (r1 + r2) * vin
 
+def stddevr(r, tolerance):
+	return (tolerance / 100 * r) / (3 / 5 * tolerance)
+
 def randrnorm(r, stddev):
 	return np.random.normal(loc=r, scale=stddev, size=1)[0]
