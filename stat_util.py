25c415ac7d44df429f921046178bdb37752e6912
Jesse <jessevrieling@gmail.com>
2025-11-09 15:26:24 +0100

    Added cp and p_factor function

--- stat_util.py
+++ stat_util.py
@@ -3,8 +3,14 @@ import numpy as np
 def stddevr(r, tolerance):
 	return tolerance / 100 * r / 3
 
+def percstddev(r, stddevr):
+	return 300 * stddevr / r
+
 def randrnorm(r, stddev):
 	return np.random.normal(loc=r, scale=stddev, size=1)[0]
 
-def cp(lsl, usl, stddev):
-	return (usl - lsl) / (6 * stddev)
+def cp(tol_des, stddev):
+	return tol_des / (3 * stddev)
+
+def p_factor(cp_acq, cp_des):
+	return cp_acq / cp_des
