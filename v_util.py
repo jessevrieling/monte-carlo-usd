dd2e900e74ce2cf0c1b57a6ac38a0767e5206291
Jesse <jessevrieling@gmail.com>
2025-11-09 15:28:35 +0100

    Added v_util

--- v_util.py
+++ v_util.py
@@ -1,2 +1,15 @@
+import stat_util
+
 def vdivout(vin, r1, r2):
 	return r2 / (r1 + r2) * vin
+
+def voltdivout(vin, r1_nom, r2_nom, stddevr1, stddevr2, n):
+	vout = []
+
+	for i in range(n):
+		r1 = stat_util.randrnorm(r1_nom, stddevr1)
+		r2 = stat_util.randrnorm(r2_nom, stddevr2)
+
+		vout.append(vdivout(vin, r1, r2))
+
+	return vout
